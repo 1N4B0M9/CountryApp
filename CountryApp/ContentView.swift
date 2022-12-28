894af2614122b1dac6a5dcc5c9cd4dd14e59f155
Nathan Morelli <2005Nathan2005@gmail.com>
2022-12-28 12:05:00 -0500

    Initial Commit

---
+++ CountryApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CountryApp
+//
+//  Created by Nathan Morelli (student LM) on 12/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by Nathan Morelli (student LM) on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
